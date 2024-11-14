4b1d5ef6c1e204b485e1d25744ceb1637c72202f
EthanGoetsch <ethangoetschhome@gmail.com>
2024-11-14 14:31:51 +0100

    Created Demo and Environment scenes with FPSSpawnPoint

---
+++ CS Project 2-1/Assets/Scripts/FPSSystem/TrainingSystem/FPSSpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FPSSystem.TrainingSystem
+{
+    public class FPSSpawnPoint : MonoBehaviour
+    {
+        [Required]
+        public Transform Agent1Spawn;
+
+        [Required]
+        public Transform Agent2Spawn;
+    }
+}
