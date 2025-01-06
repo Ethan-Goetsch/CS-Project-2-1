00a659433643e5911030293a5ba145bd6545a0d8
Cioc Alexandru <alexc.forbusiness@gmail.com>
2025-01-06 17:07:18 +0100

    Update launch_venv+train.bat

--- launch_venv+train.bat
+++ launch_venv+train.bat
@@ -1,5 +1,5 @@
 call conda init
 call conda activate mlagents
 echo Successfully launched virtual environment.
-call mlagents-learn
-cmd /k
+call mlagents-learn --run-id ppo "D:\unity\proj\config\ppo\FPS.yaml" --env="D:\unity\proj\CS Project 2-1\exec\UnityEnvironment.exe" --torch-device cuda
+PAUSE
